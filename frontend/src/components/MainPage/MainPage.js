57871270cfb6ef9953ed97c818cb458dfbdd194f
harivmasoor <harimasoor@gmail.com>
2023-08-28 12:21:55 -0700

    user auth

---
+++ frontend/src/components/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+function MainPage() {
+    return (
+      <>
+        <p>BudgetBuddy</p>
+        <footer>
+          Copyright &copy; 2023 BudgetBuddy
+        </footer>
+      </>
+    );
+  }
+  
+  export default MainPage;
--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -34,7 +34,7 @@ function NavBar () {
 
   return (
     <>
-      <h1>Chirper</h1>
+      <h1>BudgetBuddy</h1>
       { getLinks() }
     </>
   );
