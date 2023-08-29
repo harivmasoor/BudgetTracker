ab2c1ef6a9bbeead4edd1e16831ec65e081de7e4
harivmasoor <harimasoor@gmail.com>
2023-08-29 14:10:18 -0700

    fixed login redirection

--- frontend/src/components/SessionForms/LoginForm.js
+++ frontend/src/components/SessionForms/LoginForm.js
@@ -1,59 +1,82 @@
-import { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import './SessionForm.css';
 
 import { login, clearSessionErrors } from '../../store/session';
 
-function LoginForm () {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const errors = useSelector(state => state.errors.session);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    return () => {
-      dispatch(clearSessionErrors());
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
     };
-  }, [dispatch]);
-
-  const update = (field) => {
-    const setState = field === 'email' ? setEmail : setPassword;
-    return e => setState(e.currentTarget.value);
+    this.update = this.update.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  const handleSubmit = (e) => {
+  componentWillUnmount() {
+    this.props.clearSessionErrors();
+  }
+
+  update(field) {
+    return e => this.setState({ [field]: e.currentTarget.value });
+  }
+
+  handleSubmit(e) {
     e.preventDefault();
-    dispatch(login({ email, password })); 
+    const { email, password } = this.state;
+    this.props.login({ email, password }).then(success => {
+      if (success) {
+        this.props.history.push('/'); // Redirect to main page
+      }
+    });
   }
 
-  return (
-    <form className="session-form" onSubmit={handleSubmit}>
-      <h2>Log In Form</h2>
-      <div className="errors">{errors?.email}</div>
-      <label>
-        <span>Email</span>
-        <input type="text"
-          value={email}
-          onChange={update('email')}
-          placeholder="Email"
+  render() {
+    const { errors } = this.props;
+    const { email, password } = this.state;
+
+    return (
+      <form className="session-form" onSubmit={this.handleSubmit}>
+        <h2>Log In Form</h2>
+        <div className="errors">{errors?.email}</div>
+        <label>
+          <span>Email</span>
+          <input type="text"
+            value={email}
+            onChange={this.update('email')}
+            placeholder="Email"
+          />
+        </label>
+        <div className="errors">{errors?.password}</div>
+        <label>
+          <span>Password</span>
+          <input type="password"
+            value={password}
+            onChange={this.update('password')}
+            placeholder="Password"
+          />
+        </label>
+        <input
+          type="submit"
+          value="Log In"
+          disabled={!email || !password}
         />
-      </label>
-      <div className="errors">{errors?.password}</div>
-      <label>
-        <span>Password</span>
-        <input type="password"
-          value={password}
-          onChange={update('password')}
-          placeholder="Password"
-        />
-      </label>
-      <input
-        type="submit"
-        value="Log In"
-        disabled={!email || !password}
-      />
-    </form>
-  );
+      </form>
+    );
+  }
 }
 
-export default LoginForm;
+const mapStateToProps = (state) => ({
+  errors: state.errors.session
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  login: (user) => dispatch(login(user)),
+  clearSessionErrors: () => dispatch(clearSessionErrors())
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(LoginForm));
+
