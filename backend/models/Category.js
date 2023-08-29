1345aec57f8155a001f5ef34a2f50a2bafb5ee90
harivmasoor <harimasoor@gmail.com>
2023-08-29 10:45:30 -0700

    categories seeded

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -7,8 +7,6 @@ const categorySchema = new Schema({
     required: true
   }
 }, {
-  // tells mongoose to add and maintain `createdAt` and `updatedAt` fields with
-  // datetime timestamps
   timestamps: true
 });
 
