259bb3730ad95f968367d58c4c1c3990466e90a3
“eliltaA” <eliltatabrham@gmail.com>
2023-08-29 13:35:50 -0700

    merging

--- frontend/src/components/Expenses/expensePieChart.js
+++ frontend/src/components/Expenses/expensePieChart.js
@@ -1,35 +1,35 @@
-import { PieChart, Pie, Cell } from 'recharts';
-import { useSelector } from 'react-redux';
+// import { PieChart, Pie, Cell } from 'recharts';
+// import { useSelector } from 'react-redux';
 
-function ExpensePieChart() {
-  const expenses = useSelector(state => state.expenses);
+// function ExpensePieChart() {
+//   const expenses = useSelector(state => state.expenses);
 
-  // Process data for pie chart
-  const data = [
-    // Example: { name: 'Fixed Expenses', value: 400 }
-    // Populate data array based on your expenses data
-  ];
+//   // Process data for pie chart
+//   const data = [
+//     // Example: { name: 'Fixed Expenses', value: 400 }
+//     // Populate data array based on your expenses data
+//   ];
 
-  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+//   const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-  return (
-    <PieChart width={400} height={400}>
-      <Pie
-        data={data}
-        cx={200}
-        cy={200}
-        labelLine={false}
-        outerRadius={80}
-        fill="#8884d8"
-        dataKey="value"
-      >
-        {
-          data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
-        }
-      </Pie>
-    </PieChart>
-  );
-}
+//   return (
+//     <PieChart width={400} height={400}>
+//       <Pie
+//         data={data}
+//         cx={200}
+//         cy={200}
+//         labelLine={false}
+//         outerRadius={80}
+//         fill="#8884d8"
+//         dataKey="value"
+//       >
+//         {
+//           data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+//         }
+//       </Pie>
+//     </PieChart>
+//   );
+// }
 
 
-export default ExpensePieChart;
+// export default ExpensePieChart;
