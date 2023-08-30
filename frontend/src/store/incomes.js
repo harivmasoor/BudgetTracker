63da9d203fd23501a72562f84485234b08f9c34c
dominicchanDmc <dominicchan.dmc@gmail.com>
2023-08-29 23:13:08 -0700

    finish delete

--- frontend/src/store/incomes.js
+++ frontend/src/store/incomes.js
@@ -2,6 +2,7 @@ import jwtFetch from './jwt.js';
 
 export const ADD_INCOME = 'incomes/ADD_INCOME';
 export const FETCH_INCOMES = 'incomes/FETCH_INCOMES';
+export const DELETE_INCOME = 'incomes/DELETE_INCOME';
 
 export const addIncomeAction = (income) => ({
     type: ADD_INCOME,
@@ -11,6 +12,11 @@ export const addIncomeAction = (income) => ({
   export const fetchIncomesAction = (incomes) => ({
     type: FETCH_INCOMES,
     payload: incomes
+  });
+
+  export const deleteIncomeAction = (deletedIncomeId) => ({
+    type: DELETE_INCOME,
+    deletedIncomeId
   });
 
   export const addIncome = (incomeData) => {
@@ -39,7 +45,17 @@ export const addIncomeAction = (income) => ({
     };
   };
   
+  export const deletedIncome = (deletedIncomeId) => async (dispatch) => {
+    try {
+    await jwtFetch(`/api/incomes/${deletedIncomeId}`, {
+        method: 'DELETE'
+    });
 
+    dispatch(deleteIncomeAction(deletedIncomeId));
+    } catch (error) {
+    console.error('Error deleting income:', error);
+    }
+  };  
   const initialState = [];
 
   const incomesReducer = (state = initialState, action) => {
@@ -48,9 +64,11 @@ export const addIncomeAction = (income) => ({
         return [...state, action.payload];
       case FETCH_INCOMES:
         return action.payload;
+      case DELETE_INCOME:
+          return state.filter((income) => income._id !== action.deletedIncomeId);
       default:
-        return state;
-    }
+          return state;
+      }
   };
   
   export default incomesReducer;
