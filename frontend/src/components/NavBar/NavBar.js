b633fdf78e05bfd65d6d763982afcd7982be5db1
farivar <farivaramiri@gmail.com>
2023-08-29 21:49:50 -0700

    no one gives a shit about details??

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,31 +1,39 @@
-// src/components/NavBar/NavBar.js
-
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import './NavBar.css';
 import { logout } from '../../store/session';
 
-function NavBar () {
+function NavBar() {
   const loggedIn = useSelector(state => !!state.session.user);
   const dispatch = useDispatch();
-  
+  const location = useLocation();
+
   const logoutUser = e => {
-      e.preventDefault();
-      dispatch(logout());
+    e.preventDefault();
+    dispatch(logout());
   }
 
   const getLinks = () => {
-    if (loggedIn) {
+    if (loggedIn && location.pathname !== '/') {
       return (
         <div className="links-nav">
           <Link to={'/'}>Home</Link>
-          <Link to={'/mainform'}>MainForm</Link>
+          <Link to={'/incomes'}>Incomes</Link>
+          <Link to={'/expenses'}>Expenses</Link>
+          <Link to={'/budget'}>Budget</Link>
           <Link to={'/profile'}>Profile</Link>
-          <button onClick={logoutUser}>Logout</button>
+          <Link to={'/'} onClick={logoutUser}>Logout</Link>
         </div>
       );
     }
   }
+
+  return (
+    <>
+      { getLinks() }
+    </>
+  );
 }
 
 export default NavBar;
