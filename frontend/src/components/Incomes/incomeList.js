7e028713e1056ece3c008743e50a366681cd51d8
harivmasoor <harimasoor@gmail.com>
2023-08-30 09:24:55 -0700

    push

--- frontend/src/components/Incomes/incomeList.js
+++ frontend/src/components/Incomes/incomeList.js
@@ -3,12 +3,12 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fetchIncomes ,deletedIncome} from '../../store/incomes';
 import { fetchIncomeCategories } from '../../store/incomeCategories';
 import React from 'react';
-import { useSelector } from 'react-redux';
 import './Income.css';
 
 function IncomeList() {
   const incomes = useSelector(state => state.incomes);
   const incomeCategories = useSelector(state => state.incomeCategories);
+  const dispatch = useDispatch();
 
   const formattedDate = (date) => {
     return date.split('T')[0]; // Extract the date part
@@ -42,30 +42,5 @@ function IncomeList() {
     </div>
   );
 }
-
-  function IncomeList() {
-    const incomes = useSelector(state => state.incomes);
-    const incomeCategories = useSelector(state => state.incomeCategories);
-  
-    return (
-      <div className="income-list-container">
-        {incomes.map(income => (
-          <div key={income._id} className="income-item">
-            <p><strong>Income Source:</strong> {income.incomesource}</p>
-            <p><strong>Income Amount:</strong> ${income.incomeamount}</p>
-            <p><strong>Notes:</strong> {income.notes}</p>
-            <p><strong>Category:</strong> 
-              {income.category ? incomeCategories.filter(category => 
-                category._id === income.category).map(filteredCategory => filteredCategory.name) : 'N/A'}
-            </p>
-            <p><strong>Date:</strong> {income.date}</p>
-            <hr />
-          </div>
-        ))}
-      </div>
-    );
-  }
-  };
-  
 
 export default IncomeList;
