c32a7b92d100b8c96735adadfdc72740b5af77f2
farivar <farivaramiri@gmail.com>
2023-08-29 19:03:15 -0700

    fixed it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,9 @@
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Switch } from 'react-router-dom';
-
 import { AuthRoute, ProtectedRoute } from './components/Routes/routes';
 import NavBar from './components/NavBar/NavBar';
-
 import MainPage from './components/MainPage/MainPage';
-import LoginForm from './components/SessionForms/LoginForm';
-import SignupForm from './components/SessionForms/SignUpForm';
 import Profile from './components/Profile/Profile';
 import ExpenseInput from './components/Expenses/expenseInput';
 import ExpenseList from './components/Expenses/expenseList';
@@ -15,6 +11,7 @@ import ExpensePieChart from './components/Expenses/expensePieChart';
 import ExpensesPage from './components/Expenses/expensesPage';
 import { getCurrentUser } from './store/session';
 import Budget from './components/Budget/budget';
+import './App.css';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -26,12 +23,9 @@ function App() {
 
   return loaded && (
     <>
-    
       <NavBar />
       <Switch>
         <AuthRoute exact path="/" component={MainPage} />
-        <AuthRoute exact path="/login" component={LoginForm} />
-        <AuthRoute exact path="/signup" component={SignupForm} />
         <ProtectedRoute exact path="/profile" component={Profile} />
         <ProtectedRoute exact path="/expenses" component={ExpensesPage} />
         <ProtectedRoute exact path="/budget" component={Budget} />
