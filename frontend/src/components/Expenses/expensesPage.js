4e31aaf5d0a05a7c31b3d8e937e0040eb5d3a954
farivar <farivaramiri@gmail.com>
2023-08-31 10:49:23 -0700

    modal make sense here

--- frontend/src/components/Expenses/expensesPage.js
+++ frontend/src/components/Expenses/expensesPage.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ExpenseInput from './expenseInput';
 import ExpenseList from './expenseList';
 import ExpensePieChart from './expensePieChart';
+import Modal from '../Modal/Modal';
+import { useState } from 'react';
 
 function ExpensesPage() {
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
   return (
     <div className="expenses-page-container">
       <h2>Your Expenses</h2>
-      <ExpenseInput />
+      <button onClick={openModal}>Add New Expenses</button>
+      <Modal showModal={showModal} closeModal={closeModal}>
+        <ExpenseInput />
+      </Modal>
       <ExpensePieChart />
       <ExpenseList />
     </div>
