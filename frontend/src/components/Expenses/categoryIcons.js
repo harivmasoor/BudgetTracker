4b819ea1c33453824dd940821567d330f44add73
harivmasoor <harimasoor@gmail.com>
2023-09-02 11:35:08 -0700

    fixed expense icons

---
+++ frontend/src/components/Expenses/categoryIcons.js
@@ -0,0 +1,45 @@
+import Groceries from '../../assets/expensesIcons/Groceries.png';
+import DiningOut from '../../assets/expensesIcons/Dining_Out.png';
+import RentMortgage from '../../assets/expensesIcons/Rent:Mortgage.png';
+import Utilities from '../../assets/expensesIcons/Utilities.png';
+import GasFuel from '../../assets/expensesIcons/Gas:Fuel.png';
+import PublicTransport from '../../assets/expensesIcons/Public_Transport.png';
+import Healthcare from '../../assets/expensesIcons/Healthcare.png';
+import Insurance from '../../assets/expensesIcons/Insurance.png';
+import Entertainment from '../../assets/expensesIcons/Entertainment.png';
+import Clothing from '../../assets/expensesIcons/Clothing.png';
+import Education from '../../assets/expensesIcons/Education.png';
+import PersonalCare from '../../assets/expensesIcons/Personal_Care.png';
+import GiftsDonations from '../../assets/expensesIcons/Gifts:Donations.png';
+import Electronics from '../../assets/expensesIcons/Electronics.png';
+import VacationTravel from '../../assets/expensesIcons/Vacation:Travel.png';
+import Pets from '../../assets/expensesIcons/Pets.png';
+import HomeMaintenance from '../../assets/expensesIcons/Home_Maintenance.png';
+import Savings from '../../assets/expensesIcons/Savings.png';
+import Investments from '../../assets/expensesIcons/Investments.png';
+import DebtPayment from '../../assets/expensesIcons/Debt_Payment.png';
+
+const CategoryIcons = {
+    'Clothing': Clothing,
+    'Education': Education,
+    'Electronics': Electronics,
+    'Entertainment': Entertainment,
+    'Gas/Fuel': GasFuel,
+    'Gifts/Donations': GiftsDonations,
+    'Groceries': Groceries,
+    'Healthcare': Healthcare,
+    'Home Maintenance': HomeMaintenance,
+    'Insurance': Insurance,
+    'Investments': Investments,
+    'Personal Care': PersonalCare,
+    'Pets': Pets,
+    'Public Transport': PublicTransport,
+    'Rent/Mortgage': RentMortgage,
+    'Savings': Savings,
+    'Utilities': Utilities,
+    'Vacation/Travel': VacationTravel,
+    'Debt Payment': DebtPayment,
+    'Dining Out': DiningOut,
+}
+
+export default CategoryIcons;
