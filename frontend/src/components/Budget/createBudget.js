bf6d6eb7c1549513232791c4d01dfcfe9fcdba4f
dominicchanDmc <dominicchan.dmc@gmail.com>
2023-09-11 16:21:55 -0700

    finish budget

--- frontend/src/components/Budget/createBudget.js
+++ frontend/src/components/Budget/createBudget.js
@@ -9,7 +9,8 @@ function CreateBudget({chartTimeFrame, setChartTimeFrame,closeModal}) {
   const currentUser = useSelector(state => state.session.user);
   const categories = useSelector(state => state.categories);
   const [selectedInterval, setSelectedInterval] = useState('monthly'); // Default value is 'monthly'
-  const [validationError, setValidationError] = useState({});
+  const [validationError, setValidationError] = useState({}); 
+  // const budgets_month = useSelector(state => state.budget.monthly); 
 
   const [newBudget, setNewBudget] = useState({
     budgetAmount: '',
@@ -24,7 +25,8 @@ function CreateBudget({chartTimeFrame, setChartTimeFrame,closeModal}) {
     planningInterval:selectedInterval
   });
 
-  const handleCreateBudget = () => {
+  const handleCreateBudget = (e) => {
+    e.preventDefault();
     if (parseFloat(newBudget.budgetAmount) <= 0) {
       setValidationError({ budgetAmount: "Budget amount must be greater than 0" });
     } else {
@@ -65,81 +67,84 @@ function CreateBudget({chartTimeFrame, setChartTimeFrame,closeModal}) {
   return (
     <div className="create-budget-form">
        <h2>Add New Budget</h2>
-      <div>
-        <label>
-          Budget Plan:
-          <input
-            type="string"
-            value={newBudget.budgetPlan}
-            onChange={(e) =>
-              setNewBudget({ ...newBudget, budgetPlan: e.target.value })
-            }
-            required
-            />
-        </label>
-        <label>
-          Budget Amount:
-          <div className="errors">{validationError?.budgetAmount}</div>
-          <input
-            type="number"
-            value={newBudget.budgetAmount}
-            onChange={(e) =>
-              setNewBudget({ ...newBudget, budgetAmount: e.target.value })
-            }
-            required
-            />
-        </label>
-        <label>
-          Date:
-          <input
-            type="date"
-            value={newBudget.date}
-            onChange={(e) =>
-              setNewBudget({ ...newBudget, date: e.target.value })
-            }
-            required
-            />
-        </label>
-        <label>
-          Budget Note:
-          <input
-            type="string"
-            value={newBudget.notes}
-            onChange={(e) =>
-              setNewBudget({ ...newBudget, notes: e.target.value })
-            }
-            />
-        </label>
-        <label>
-          Budget Category:
-          <select 
-          id="category" 
-          name="category" 
-          value={newBudget.category} 
-          onChange={(e) =>
-            setNewBudget({ ...newBudget, category: e.target.value })
-          }
-          required
-          >
-          <option value="" disabled>Select a category</option>
-          {categories.map((category, index) => (
-            <option key={index} value={category._id}>{category.name}</option>
-            ))}
-        </select>
-        </label>
-        <label>
-            Select Planning Interval:
-            <select
-              value={selectedInterval}
-              onChange={(e) => setSelectedInterval(e.target.value)}
-              >
-              <option value="monthly">Monthly</option>
-              <option value="yearly">Yearly</option>
-            </select>
+       <form onSubmit={handleCreateBudget}>
+        <div>
+          <label>
+            Budget Plan:
+            <input
+              type="string"
+              value={newBudget.budgetPlan}
+              onChange={(e) =>
+                setNewBudget({ ...newBudget, budgetPlan: e.target.value })
+              }
+              required
+              />
           </label>
-        {/* Other input fields */}
-        <button onClick={handleCreateBudget}>Add Budget</button>
-      </div>
+          <label>
+            Budget Amount:
+            <div className="errors">{validationError?.budgetAmount}</div>
+            <input
+              type="number"
+              value={newBudget.budgetAmount}
+              onChange={(e) =>
+                setNewBudget({ ...newBudget, budgetAmount: e.target.value })
+              }
+              required
+              />
+          </label>
+          <label>
+            Date:
+            <input
+              type="date"
+              value={newBudget.date}
+              onChange={(e) =>
+                setNewBudget({ ...newBudget, date: e.target.value })
+              }
+              required
+              />
+          </label>
+          <label>
+            Budget Note:
+            <input
+              type="string"
+              value={newBudget.notes}
+              onChange={(e) =>
+                setNewBudget({ ...newBudget, notes: e.target.value })
+              }
+              />
+          </label>
+          <label>
+            Budget Category:
+            <select 
+            id="category" 
+            name="category" 
+            value={newBudget.category} 
+            onChange={(e) =>
+              setNewBudget({ ...newBudget, category: e.target.value })
+            }
+            required
+            >
+            <option value="" disabled>Select a category</option>
+            {categories.map((category, index) => (
+              <option key={index} value={category._id}>{category.name}</option>
+              ))}
+          </select>
+          </label>
+          <label>
+              Select Planning Interval:
+              <select
+                value={selectedInterval}
+                onChange={(e) => setSelectedInterval(e.target.value)}
+                >
+                <option value="monthly">Monthly</option>
+                <option value="yearly">Yearly</option>
+              </select>
+            </label>
+          {/* Other input fields */}
+          <button >Add Budget</button>
+        </div>
+
+       </form>
     </div>
   );
 }
