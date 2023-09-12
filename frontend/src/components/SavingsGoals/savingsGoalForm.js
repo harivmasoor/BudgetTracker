50feef8341ed85ef3cca712bde6cedcbb945b0f2
eliltaA <eliltatabrham@gmail.com>
2023-09-11 18:03:10 -0700

    savings

--- frontend/src/components/SavingsGoals/savingsGoalForm.js
+++ frontend/src/components/SavingsGoals/savingsGoalForm.js
@@ -5,14 +5,24 @@ import Modal from '../Modal/Modal';
 import SavingsGoalList from './savingsGoalList';
 
 const SavingsGoalForm = () => {
+  // const [showModal, setShowModal] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  // const openModal = () => setShowModal(true);
+  // const closeModal = () => setShowModal(false);
   const [goalAmount, setGoalAmount] = useState('');
   const [currentAmount, setCurrentAmount] = useState('');
   const [notes, setNotes] = useState('');
+  const [validationError, setValidationError] = useState({});
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // setValidationError({})
+    if (parseFloat(goalAmount) <= 0){
+      setValidationError({goalAmount: "Goal amount must be greater than 0"});
+    } else if (parseFloat(currentAmount) < 0){
+      setValidationError({currentAmount: "Current amount must be greater than or equal to 0"});
+    } else {
     const savingsGoalData = {
       goalAmount: parseFloat(goalAmount),
       currentAmount: parseFloat(currentAmount),
@@ -24,26 +34,42 @@ const SavingsGoalForm = () => {
     setCurrentAmount('');
     setNotes('');
     setShowModal(false); // Close modal after submitting
+    setValidationError({})
+  }
+  };
+
+  const closeModal = () => {
+    setValidationError({});
+    setGoalAmount('');
+    setCurrentAmount('');
+    setNotes(''); // Clear validation errors when closing the modal
+    setShowModal(false);
   };
 
   return (
     <div className="incomes-page-container">
       <h1>Savings Goals</h1>
       <button className="add-goal-button" onClick={() => setShowModal(true)}>Add Goal</button> 
+      {/* onClick={() => setShowModal(true)} */}
       {/* Modal */}
-      <Modal showModal={showModal} closeModal={() => setShowModal(false)}>
+
+      <Modal showModal={showModal} closeModal={closeModal}>
         <form onSubmit={handleSubmit}>
+        <div className="errors">{validationError?.goalAmount}</div>
           <input
             type="number"
             placeholder="Goal Amount"
             value={goalAmount}
             onChange={(e) => setGoalAmount(e.target.value)}
+            required
           />
+          <div className="errors">{validationError?.currentAmount}</div>
           <input
             type="number"
             placeholder="Current Amount"
             value={currentAmount}
             onChange={(e) => setCurrentAmount(e.target.value)}
+            required
           />
           <input
             type="text"
