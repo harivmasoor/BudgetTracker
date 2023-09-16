824fea4cf1c12bb0ab96a9192ca55181a85eba47
harivmasoor <harimasoor@gmail.com>
2023-09-15 21:25:01 -0700

    updated Mongo password

--- backend/seeders/seed.js
+++ backend/seeders/seed.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 const Category = require('../models/Category'); // Update this path to point to your actual Category model file
 const IncomeCategory = require('../models/IncomeCategory');
@@ -5,7 +7,7 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://admin:tfPoFeVmBbKLLWff@budgetbuddy.zqn6bvq.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
