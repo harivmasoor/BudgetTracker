59a20d7ed48b34f8edfa6bfe40cf9a6c81330430
harivmasoor <harimasoor@gmail.com>
2023-09-15 21:06:44 -0700

    open about me links in new tab

--- frontend/src/components/ContactUs/ContactUsPage.js
+++ frontend/src/components/ContactUs/ContactUsPage.js
@@ -13,9 +13,9 @@ function ContactUsPage() {
         <h1>Elilta Abrham</h1>
         <img src={elilta} alt="elilta" />
         <div className="icons-container">
-          <a  href="https://github.com/eliltaA" ><i class="fa-brands fa-github"></i></a>
+          <a  href="https://github.com/eliltaA" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i></a>
           <a  href="eliltatabrham@gmail.com" ><i class="fa fa-user-circle"></i></a>
-          <a  href="https://www.linkedin.com/in/elilta-abrham-87b79228b/" ><i class="fa-brands fa-linkedin"></i></a>
+          <a  href="https://www.linkedin.com/in/elilta-abrham-87b79228b/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin"></i></a>
           <div className="about-me">
           I'm a dedicated software engineer with a passion for coding that goes back to my high school days. I've journeyed from a background in customer service to mastering languages like JavaScript and Ruby and creating Full-Stack Projects. I enjoy tackling challenges head-on and thrive in collaborative environments. Let's build something amazing together.
           </div>
@@ -26,9 +26,9 @@ function ContactUsPage() {
         <h1>Dominic Chan</h1>
         <img src={dominic} alt="dominic" />
         <div className="icons-container">
-          <a  href="https://github.com/dominicchanDmc" ><i class="fa-brands fa-github"></i></a>
-          <a  href="https://dominicchan.netlify.app/" ><i class="fa fa-user-circle"></i></a>
-          <a  href="https://www.linkedin.com/in/dominic-chan-0b4128255" ><i class="fa-brands fa-linkedin"></i></a>
+          <a  href="https://github.com/dominicchanDmc" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i></a>
+          <a  href="https://dominicchan.netlify.app/" target="_blank" rel="noopener noreferrer"><i class="fa fa-user-circle"></i></a>
+          <a  href="https://www.linkedin.com/in/dominic-chan-0b4128255" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin"></i></a>
           <div className="about-me">
           Hi, I am Dominic Chan, Having worked as a Full-Stack Developer for seven years in Hong Kong, mainly take part in government and hospital project. During that time, I built different system for public use , using Java, SQL, and Javascript, Jquery. I loved the feeling during debug and the sense of success after solve the bug.
 Now, I live in USA and learn new skill such as React, Redux, Ruby on Rails.
@@ -41,9 +41,9 @@ I'm currently looking for a job where I can make an impact, and using my debuggi
         <h1>Hari Masoor</h1>
         <img src={hari} alt="hari" />
         <div className="icons-container">
-          <a  href="https://github.com/harivmasoor" ><i class="fa-brands fa-github"></i></a>
-          <a  href="https://harimasoor.com" ><i class="fa fa-user-circle"></i></a>
-          <a  href="https://www.linkedin.com/in/harimasoor/" ><i class="fa-brands fa-linkedin"></i></a> 
+          <a  href="https://github.com/harivmasoor" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i></a>
+          <a  href="https://harimasoor.com" target="_blank" rel="noopener noreferrer"><i class="fa fa-user-circle"></i></a>
+          <a  href="https://www.linkedin.com/in/harimasoor/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin"></i></a> 
           <div className="about-me">
           Hi, I am Hari Masoor. My experiences range from customer service and
 troubleshooting...to technically validating 8 figure deals and delivering global keynotes at the
@@ -57,9 +57,9 @@ it. I am currently seeking to apply my unique skill set to fullstack positions.
         <h1>Farivar Amiri</h1>
         <img src={farivar} alt="farivar" />
         <div className="icons-container">
-          <a  href="https://github.com/Farivar90" ><i class="fa-brands fa-github"></i></a>
-          <a  href="https://farivar-amiri.com" ><i class="fa fa-user-circle"></i></a>
-          <a  href="https://linkedin.com/in/farivar-amiri-458273198" ><i class="fa-brands fa-linkedin"></i></a>
+          <a  href="https://github.com/Farivar90" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i></a>
+          <a  href="https://farivar-amiri.com" target="_blank" rel="noopener noreferrer"><i class="fa fa-user-circle"></i></a>
+          <a  href="https://linkedin.com/in/farivar-amiri-458273198" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin"></i></a>
           <div className="about-me">
           Hello,
 
